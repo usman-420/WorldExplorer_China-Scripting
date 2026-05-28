301b7fd2c52fda795e2a1c3f351da167af192178
usman-420 <r0916150@student.thomasmore.be>
2026-05-28 21:04:19 +0200

    remove test.py from tracking

--- China/test.py
+++
@@ -1,5 +0,0 @@
-from WorldExplorer.traveller_profile import traveller_profile
-from WorldExplorer.match_destinations import match_destinations
-
-profile = traveller_profile()
-df = match_destinations(profile)
